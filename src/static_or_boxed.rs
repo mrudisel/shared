6efcd51068b733a0c60aff75f22d56bbcb5627d7
mrudisel <mitchrudisel@gmail.com>
2024-03-04 20:09:30 -0800

    added StaticOrBoxed variant

--- src/static_or_boxed.rs
+++ src/static_or_boxed.rs
@@ -1,30 +1,28 @@
-use std::{fmt, sync::Arc};
-
-pub mod static_or_boxed;
+use std::fmt;
 
 
-pub enum Shared<T: ?Sized + 'static> {
+pub enum StaticOrBoxed<T: ?Sized + 'static> {
     Static(&'static T),
-    Shared(Arc<T>)
+    Boxed(Box<T>)
 }
 
-impl<T: ?Sized> Clone for Shared<T> {
+impl<T: ?Sized + Clone> Clone for StaticOrBoxed<T> {
     fn clone(&self) -> Self {
         match self {
-            Self::Shared(shared) => Self::Shared(Arc::clone(shared)),
+            Self::Boxed(boxed) => Self::Boxed(boxed.clone()),
             Self::Static(stat) => Self::Static(stat),
         }
     }
 }
 
 
-impl<T: ?Sized> Shared<T> {
+impl<T: ?Sized> StaticOrBoxed<T> {
     pub fn is_static(&self) -> bool {
         matches!(self, Self::Static(_))
     }
 
-    pub fn is_stared(&self) -> bool {
-        matches!(self, Self::Shared(_))
+    pub fn is_boxed(&self) -> bool {
+        matches!(self, Self::Boxed(_))
     }
 
 
@@ -35,43 +33,24 @@ impl<T: ?Sized> Shared<T> {
         }
     }
     
-    pub fn as_shared(&self) -> Option<&Arc<T>> {
+    pub fn as_boxed(&self) -> Option<&Box<T>> {
         match self {
-            Self::Shared(shared) => Some(shared),
+            Self::Boxed(b) => Some(b),
             _ => None,
         }
     }
-
-    pub fn to_shared(&mut self) -> &Arc<T>
-    where 
-        Box<T>: From<&'static T>
-    {
-        match self {
-            Self::Shared(shared) => shared,
-            Self::Static(stat) => {
-                let shared = Arc::from(Box::from(stat));    
-                
-                *self = Self::Shared(shared);
-
-                match self {
-                    Self::Shared(shared) => shared,
-                    _ => unreachable!(),
-                }
-            }
-        }
-    } 
 }
 
-impl<T: ?Sized> Default for Shared<T>
+impl<T: ?Sized> Default for StaticOrBoxed<T>
 where   
-    Arc<T>: Default,
+    Box<T>: Default,
 {
     fn default() -> Self {
-        Self::Shared(Arc::default())
+        Self::Boxed(Box::default())
     }
 }
 
-impl<T: ?Sized, U> PartialEq<U> for Shared<T>
+impl<T: ?Sized, U> PartialEq<U> for StaticOrBoxed<T>
 where 
     T: PartialEq<U>,
 {
@@ -80,7 +59,7 @@ where
     }
 }
 
-impl<T: ?Sized, U> PartialOrd<U> for Shared<T>
+impl<T: ?Sized, U> PartialOrd<U> for StaticOrBoxed<T>
 where 
     T: PartialOrd<U>,
 {
@@ -93,29 +72,29 @@ where
 
 
 
-impl<T: ?Sized> AsRef<T> for Shared<T> {
+impl<T: ?Sized> AsRef<T> for StaticOrBoxed<T> {
     #[inline(always)]
     fn as_ref(&self) -> &T {
         match self {
-            Self::Shared(shared) => shared,
+            Self::Boxed(shared) => shared,
             Self::Static(stat) => stat,
         }
     }
 }
 
-impl<T: ?Sized + fmt::Debug> fmt::Debug for Shared<T> {
+impl<T: ?Sized + fmt::Debug> fmt::Debug for StaticOrBoxed<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.as_ref().fmt(f)
     }
 }
 
-impl<T: ?Sized + fmt::Display> fmt::Display for Shared<T> {
+impl<T: ?Sized + fmt::Display> fmt::Display for StaticOrBoxed<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.as_ref().fmt(f)
     }
 }
 
-impl<T: ?Sized> std::ops::Deref for Shared<T> {
+impl<T: ?Sized> std::ops::Deref for StaticOrBoxed<T> {
     type Target = T;
 
     #[inline(always)]
@@ -124,7 +103,7 @@ impl<T: ?Sized> std::ops::Deref for Shared<T> {
     }
 }
 
-impl<T: ?Sized> std::borrow::Borrow<T> for Shared<T> {
+impl<T: ?Sized> std::borrow::Borrow<T> for StaticOrBoxed<T> {
     #[inline(always)]
     fn borrow(&self) -> &T {
         self.as_ref()
@@ -132,26 +111,26 @@ impl<T: ?Sized> std::borrow::Borrow<T> for Shared<T> {
 }
 
 
-impl<T: ?Sized> From<&'static T> for Shared<T> {
+impl<T: ?Sized> From<&'static T> for StaticOrBoxed<T> {
     fn from(value: &'static T) -> Self {
         Self::Static(value)
     }
 }
 
-impl<T: ?Sized> From<Arc<T>> for Shared<T> {
-    fn from(value: Arc<T>) -> Self {
-        Self::Shared(value)
+impl<T: ?Sized> From<Box<T>> for StaticOrBoxed<T> {
+    fn from(value: Box<T>) -> Self {
+        Self::Boxed(value)
     }
 }
 
-impl From<String> for Shared<str> {
+impl From<String> for StaticOrBoxed<str> {
     fn from(value: String) -> Self {
-        Self::Shared(Arc::from(value))        
+        Self::Boxed(Box::from(value))        
     }
 }
 
 
-impl<T: ?Sized + std::hash::Hash> std::hash::Hash for Shared<T> {
+impl<T: ?Sized + std::hash::Hash> std::hash::Hash for StaticOrBoxed<T> {
     fn hash<H>(&self, state: &mut H) 
     where   
         H: std::hash::Hasher,
@@ -161,7 +140,7 @@ impl<T: ?Sized + std::hash::Hash> std::hash::Hash for Shared<T> {
 }
 
 #[cfg(feature = "serde")]
-impl<T: ?Sized + serde::Serialize> serde::Serialize for Shared<T> {
+impl<T: ?Sized + serde::Serialize> serde::Serialize for StaticOrBoxed<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer 
@@ -171,7 +150,7 @@ impl<T: ?Sized + serde::Serialize> serde::Serialize for Shared<T> {
 }
 
 #[cfg(feature = "serde")]
-impl<'de, T: ?Sized> serde::Deserialize<'de> for Shared<T> 
+impl<'de, T: ?Sized> serde::Deserialize<'de> for StaticOrBoxed<T> 
 where 
     Box<T>: serde::Deserialize<'de>,
 {
@@ -180,6 +159,6 @@ where
         D: serde::Deserializer<'de> 
     {
         let boxed = Box::<T>::deserialize(deserializer)?;
-        Ok(Self::Shared(Arc::from(boxed)))        
+        Ok(Self::Boxed(boxed))        
     }
 }
